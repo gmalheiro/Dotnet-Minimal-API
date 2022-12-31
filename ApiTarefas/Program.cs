5d064fedacdab889cb61400cf2a29c13674ea89a
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-31 16:27:03 -0300

    Added Get and post  methods, with mocked data

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,36 +1,53 @@
-namespace ApiTarefas
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args); // [5] builder.Services.AddAuthorization();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TarefasDB"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+app.MapGet("/HelloWorld", () => "Hello,World CSharp  asp .Net core web minimal API");
 
-            var app = builder.Build();
+app.MapGet("/Frases", async () =>
+    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    );
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+app.MapGet("/Tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
 
-            app.MapGet("/HelloWorld", () => "Hello,World CSharp  asp .Net core web minimal API");
+app.MapPost("/Tarefas", async (AppDbContext db, Tarefa tarefa) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Tarefas/{tarefa.Id}", tarefa);
+});
 
-            app.MapGet("/Frases",async () => 
-                await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
-                );
+//app.UseAuthorization();
 
-            app.UseAuthorization();
+app.Run();
 
-            app.Run();
-        }
-    }
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
 }
